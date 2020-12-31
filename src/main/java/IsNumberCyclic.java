d2286a1b17f841e2507bb0a154febd63cd0f2c0e
greedo85 <qiliveadata@gmail.com>
2020-12-31 17:00:48 +0100

    rozwiązanie w wersji sprawdzającej pierwszy wynik mnożenia

---
+++ src/main/java/IsNumberCyclic.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class IsNumberCyclic {
+
+    public static void main( String[] args ) {
+        isCyclic(142857);
+        //125874*2=251748
+    }
+
+    public static boolean isCyclic( long number ) {
+        String nr = String.valueOf(number);
+        List<String> multiplied = new ArrayList<>();
+        boolean check = false;
+        for (int i = 2; i <= 100; i++) {
+            long temp = number * i;
+            if (String.valueOf(temp).length() > nr.length()) {
+                break;
+            }
+            multiplied.add(String.valueOf(temp));
+        }
+        System.out.println("tablica permutacji");
+        for (String s : multiplied) {
+            System.out.println(s);
+        }
+
+        for (int j = 0; j < multiplied.get(0).length(); j++) {
+            for (int k = 0; k < nr.length(); k++) {
+                if (multiplied.get(0).charAt(j) == nr.charAt(k)) {
+                    check = true;
+                }
+            }
+        }
+        
+        return check;
+    }
+}
--- src/test/java/IsCyclicTest.java
+++ src/test/java/IsCyclicTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class IsCyclicTest {
+
+    @Test
+    public void isCyclicTest() {
+        //given
+
+        IsNumberCyclic isNumberCyclic = new IsNumberCyclic();
+
+        long number = 142857;
+        //then
+        assertTrue(isNumberCyclic.isCyclic(number));
+
+
+    }
 }
